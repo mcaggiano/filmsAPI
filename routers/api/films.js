d9ab70a1f49cb600fa1452f76e8a5a9b4ebf0879
mcaggiano <caggianomauricio@gmail.com>
2021-12-08 04:06:31 -0300

    Check create film

--- routers/api/films.js
+++ routers/api/films.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const { check, validationResult} = require('express-validator')
 const { Film } = require("../../db")
 
 router.get('/', async (req, res) => {
@@ -7,10 +7,28 @@ router.get('/', async (req, res) => {
     res.json(films)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', [
+    check('title', 'La película debe tener un titulo').not().isEmpty(),
+    check('description', 'La descripción debe tener entre 10 y 100 carácteres').isLength({max: 100, min: 10}),
+    check('scort', 'El campo score no puede estar vacio').not().isEmpty(),
+    check('director', 'La pelicula debe tener un director').not().isEmpty().isLength({min: 3, max: 30})
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ errores: errors.array()})
+    }
+
     const film = await Film.create(req.body)
     res.json(film)
 })
+
+router.get('/:filmId', async (req, res) => {
+    const film = await Film.findOne({where : {id: req.params.filmId}})
+    res.json(film)
+    
+})
+
 
 router.put('/:filmId', async (req,res) => {
     await Film.update(req.body, {
